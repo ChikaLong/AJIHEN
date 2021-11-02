eb1eb64691b58e54b3527ec14f1c7238905a51f3
ChikaLong <longb.0818@gmail.com>
2021-11-02 11:45:12 +0000

    [Update]非同期化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,16 @@
 class CommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    comment = Comment.new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
     comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
-    redirect_to post_path(post)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
-    comment.destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+    @comment.destroy
   end
 
   private
