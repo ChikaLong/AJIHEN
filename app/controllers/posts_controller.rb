c35ac4bcc6afaafa742001e49015e9c9b0f670b1
ChikaLong <longb.0818@gmail.com>
2021-11-04 00:00:17 +0000

    [Update]コードの整理

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,24 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:show, :edit, :update, :destroy]
+
   def index
     if params[:sort_create]
-      # 新着順
       @posts = Post.latest.page(params[:page]).per(20)
     elsif params[:sort_rate]
-      # 評価順
       @posts = Post.rating.page(params[:page]).per(20)
     elsif params[:sort_comment]
-      # コメント数順
       @posts = Kaminari.paginate_array(Post.many).page(params[:page]).per(20)
     elsif params[:sort_favorite]
-      # いいね数順
       @posts = Kaminari.paginate_array(Post.like).page(params[:page]).per(20)
     else
       @posts = Post.page(params[:page]).per(20)
     end
-    @today_ranks = Post.find(Favorite.group(:post_id).where(created_at: Time.current.all_day).order('count(post_id) desc').limit(3).pluck(:post_id))
-    @week_ranks = Post.find(Favorite.group(:post_id).where(created_at: Time.current.all_week).order('count(post_id) desc').limit(3).pluck(:post_id))
-    @month_ranks = Post.find(Favorite.group(:post_id).where(created_at: Time.current.all_month).order('count(post_id) desc').limit(3).pluck(:post_id))
+    @today_ranks = Post.today
+    @week_ranks = Post.week
+    @month_ranks = Post.month
   end
 
   def show
-    @post = Post.find(params[:id])
     @user = @post.user
     @comment = Comment.new
   end
@@ -43,13 +40,11 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
     @tags = Tag.all
     @categories = Category.all
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to post_path(@post), notice: 'レビュー内容を更新しました'
     else
@@ -58,12 +53,15 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: 'レビューを削除しました'
   end
 
   private
+  def set_post
+     @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:image, :item_name, :review, :country, :place, :price, :rate, :category_id, { tag_ids: [] })
   end
