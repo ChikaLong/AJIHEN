e257ce638d4881809d161010afec978d61532828
ChikaLong <longb.0818@gmail.com>
2021-11-04 04:38:55 +0000

    [Add]パスワード再設定機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -58,4 +58,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  host = 'e9c4f870664548339e5038dc7abc6271.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port => 587,
+    :address => "smtp.gmail.com",
+    :domain => "gmail.com",
+    :user_name => ENV["MAIL_ADDRESS"],
+    :password => ENV["MAIL_PASSWORD"],
+    :authentication => 'login',
+    :enable_starttls_auto => true
+  }
 end
