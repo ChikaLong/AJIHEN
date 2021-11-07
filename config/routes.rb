68fa2254d459546fec84314536cbc00256cdf75b
ChikaLong <longb.0818@gmail.com>
2021-11-07 07:36:33 +0000

    [Add]登録情報編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
 
   get '/about' => 'homes#about'
 
