52254b3889154868a51681ffeb5a4d4c61ee5d22
ChikaLong <longb.0818@gmail.com>
2021-11-08 01:14:18 +0000

    [Add]登録完了メール

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    if @user.save
+      WelcomeMailer.welcome_mail(params[:user][:email], params[:user][:name]).deliver
+    end
+  end
 
   # GET /resource/edit
   # def edit
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: '"AJIHEN"<from@example.com>'
   layout 'mailer'
 end
