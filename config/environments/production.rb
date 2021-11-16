fa21214d224106a46a22b031ee403259863bd6d5
ChikaLong <longb.0818@gmail.com>
2021-11-16 09:14:11 +0000

    [Add]メールの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port => 587,
+    :address => "smtp.gmail.com",
+    :domain => "gmail.com",
+    :user_name => ENV["MAIL_ADDRESS"],
+    :password => ENV["MAIL_PASSWORD"],
+    :authentication => 'login',
+    :enable_starttls_auto => true
+  }
 end
