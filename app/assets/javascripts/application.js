1a26a01e5fc36cfbd334066e457c62cd5d802558
ChikaLong <longb.0818@gmail.com>
2021-12-14 12:05:50 +0000

    [Update]Top投稿のレイアウト

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,7 +50,7 @@ $(document).on('turbolinks:load', function(){
 $(document).on('turbolinks:load', function(){
   $('.top-post').slick({
     dots: true,
-    centerMode: true,
+    slidesToShow: 3,
   });
 });
 
